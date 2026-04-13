dc0db9f0505fc5254dbb118db74c21c518f25794
UniPM <zoc6x8voc@mozmail.com>
2026-04-12 23:26:15 -0500

    Added invite accepting & querying

--- Commands/Init.cs
+++ Commands/Init.cs
@@ -7,5 +7,7 @@ public static class Init // Function/class for initializing all the commands (fo
     public static void InitCmd() {
         FourKit.getCommand("town").setExecutor(new TownCmd());
         FourKit.getCommand("t").setExecutor(new TownCmd());
+        FourKit.getCommand("invite").setExecutor(new InvCmd());
+        FourKit.getCommand("inv").setExecutor(new InvCmd());
     }
 }
---
+++ Commands/Invite.cs
@@ -0,0 +1,46 @@
+namespace Respublica.Commands;
+
+using Minecraft.Server.FourKit.Command;
+using Minecraft.Server.FourKit.Entity;
+
+public class InvCmd : CommandExecutor // Commands for managing invites
+{
+    public bool onCommand(CommandSender sender, Command command, string label, string[] args)
+    {
+        if (sender is not Player) return true;
+
+        if (args.Length == 0)
+        {
+            sender.sendMessage("Town invites:");
+            foreach (var al0town in DBInteract.getPlr(((Player)sender).getUniqueId()).invites) sender.sendMessage(al0town.name);
+            sender.sendMessage("Do /invite accept [name] to join a town!");
+        }
+
+        if (args.Length > 1)
+        {
+            switch (args[0])
+            {
+                case "accept":
+                    if (args.Length < 2) break;
+
+                    if (DBInteract.getTown(args[1]) == null) break;
+
+                    var acplr = DBInteract.getPlr(((Player)sender).getUniqueId());
+                    if (!acplr.invites.Any(x => x.name == args[1])) break;
+
+                    acplr.invites.RemoveAll(x => x.name == args[1]);
+                    acplr.town = args[1];
+
+                    DBInteract.updatePlr(DBInteract.getPlr(((Player)sender).getUniqueId()), acplr);
+                    var actown = DBInteract.getTown(args[1]);
+                    actown.residents.Add(((Player)sender).getUniqueId());
+                    DBInteract.updateTown(DBInteract.getTown(args[1]), actown);
+
+                    sender.sendMessage($"You have joined {args[1]}!");
+
+                    break;
+            }
+        }
+        return true;
+    }
+}
